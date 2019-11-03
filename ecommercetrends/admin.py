99316f9d523c24770edacc5d33bb62628253093c
knight <ariefmaulana130598@gmail.com>
2019-11-03 18:59:15 +0700

    Model Finished

--- ecommercetrends/admin.py
+++ ecommercetrends/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import SepatuPria, SepatuWanita
+
 
 # Register your models here.
+class OrderDate(admin.ModelAdmin):
+    date_hierarchy = 'tanggal'
+    ordering = ('-tanggal',)
+    list_display = ('nama', 'jenis', 'rating', 'produk', 'tanggal', 'review')
+
+
+admin.site.register(SepatuPria, OrderDate)
+admin.site.register(SepatuWanita, OrderDate)
--- ecommercetrends/models.py
+++ ecommercetrends/models.py
@@ -2,6 +2,30 @@ from django.db import models
 
 
 # Create your models here.
+class AbstractUlasan(models.Model):
+    nama = models.TextField(null=True, blank=True)
+    jenis = models.CharField(max_length=255, null=True, blank=True)
+    rating = models.CharField(max_length=255, null=True, blank=True)
+    tanggal = models.DateField()
+    produk = models.CharField(max_length=255)
+    review = models.TextField(null=True, blank=True)
 
-class ulasan(models.Model):
-    nama = models.CharField(max_length=100)
+    def __str__(self):
+        return '{}'.format(self.nama + " - " + self.produk + " - " + self.tanggal.strftime('%B %d, %Y'))
+
+    class Meta:
+        abstract = True
+
+
+class SepatuPria(AbstractUlasan):
+    pass
+
+    class Meta:
+        verbose_name_plural = 'Sepatu Pria'
+
+
+class SepatuWanita(AbstractUlasan):
+    pass
+
+    class Meta:
+        verbose_name_plural = 'Sepatu Wanita'
