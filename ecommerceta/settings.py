8122b3a3e7ce83884ac0a535587504759afc972e
Arief Maulana <33091225+arief130598@users.noreply.github.com>
2019-12-28 05:27:40 +0700

    Update settings.py

--- ecommerceta/settings.py
+++ ecommerceta/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q-lt76)c8icxatt$z#y_3=zhhj2jmk=8n!48l$kx6s-oxq+l+('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ecommerceanalysis.azurewebsites.net']
 
